d813af942f9f5be0855bbb7a64f088dd60d910d7
Michael Akinde <michael.akinde@statnett.no>
2024-01-29 12:47:55 +0100

    Removed dead code

--- main.go
+++ main.go
@@ -10,22 +10,4 @@ var (
 
 func main() {
 	cmd.Execute(version)
-	/*
-		fmt.Println("hi world, this is the gh-devxp extension!")
-		client, err := api.DefaultRESTClient()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		response := struct{ Login string }{}
-		err = client.Get("user", &response)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	*/
-	//fmt.Printf("running as %s\n", response.Login)
 }
-
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
