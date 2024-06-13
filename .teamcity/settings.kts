5c27f01c42d2a19e6607cdd9fb72898f4ece3576
Michael Akinde <michael.akinde@statnett.no>
2024-06-13 20:44:08 +0200

    Update TC settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,22 +2,9 @@ import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubP
 import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
 import no.elhub.devxp.build.configuration.pipeline.jobs.makeVerify
 
-elhubProject(DEVXP, "devxp-jira-scripts") {
+elhubProject(DEVXP, "gh-dxp") {
 
-    params {
-<<<<<<< HEAD
-        param("env.PATH", "\$PATH:/opt/go/1.21.6/bin")
-=======
-        param("env.PATH", "/opt/go/1.21.6/bin:%teamcity.tool.maven.DEFAULT%/bin:%KOTLIN_PATH%:%env.PATH%:/home/teamcity/.nvm/versions/node/v20.10.0/bin")
->>>>>>> 6a9ae7f4911a33d665cfb99d95d183a741439e99
-        param("env.GOROOT", "/opt/go/1.21.6")
-    }
-
-    pipeline {
-        sequential {
-            makeVerify {
-                disableSonarScan = true
-            }
-        }
+    pipeline(withReleaseVersion = false) {
+        makeVerify()
     }
 }
