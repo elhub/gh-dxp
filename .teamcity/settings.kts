5bc8d6c1948a974ebffb18a450a66eb8beb518a5
Michael Akinde <michael.akinde@statnett.no>
2024-06-14 18:42:44 +0200

    Test local GO params

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,9 +2,19 @@ import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubP
 import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
 import no.elhub.devxp.build.configuration.pipeline.jobs.makeVerify
 
+
 elhubProject(DEVXP, "gh-dxp") {
 
-    pipeline(withReleaseVersion = false) {
-        makeVerify()
+    params {
+        param("env.PATH", "\$PATH:/opt/go/1.21.6/bin")
+        param("env.GOROOT", "/opt/go/1.21.6")
+    }
+
+    pipeline {
+        sequential {
+            makeVerify {
+                disableSonarScan = true
+            }
+        }
     }
 }
