b8e99eb342ad95f5af2699e2dc7ad90c698f2965
Michael Akinde <michael.akinde@statnett.no>
2024-06-14 18:54:51 +0200

    Add more path variables

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,7 +5,7 @@ import no.elhub.devxp.build.configuration.pipeline.jobs.makeVerify
 elhubProject(DEVXP, "devxp-jira-scripts") {
 
     params {
-        param("env.PATH", "\$PATH:/opt/go/1.21.6/bin")
+        param("env.PATH", "/opt/go/1.21.6/bin:%teamcity.tool.maven.DEFAULT%/bin:%KOTLIN_PATH%:%env.PATH%:/home/teamcity/.nvm/versions/node/v20.10.0/bin")
         param("env.GOROOT", "/opt/go/1.21.6")
     }
 
