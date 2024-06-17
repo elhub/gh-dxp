632711258b21811f9f6b06d167a09e20b24fdb72
Michael Akinde <michael.akinde@statnett.no>
2024-06-17 15:35:05 +0200

    Add template command to gh dxp

---
+++ pkg/template/template_test.go
@@ -0,0 +1,47 @@
+package template_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/elhub/gh-dxp/pkg/config"
+	"github.com/elhub/gh-dxp/pkg/template"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExecute(t *testing.T) {
+	// Create a temporary directory for testing
+	tempDir, err := os.MkdirTemp("", "template-test")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+
+	// Set up the test settings
+	settings := &config.Settings{
+		ProjectTemplateUri: "http://example.com/template/",
+	}
+
+	// Set up the expected file paths
+	expectedFilePaths := []string{
+		filepath.Join(tempDir, ".editorconfig"),
+		filepath.Join(tempDir, ".gitattributes"),
+		filepath.Join(tempDir, ".gitignore"),
+		filepath.Join(tempDir, "README.md"),
+		filepath.Join(tempDir, ".github/CODEOWNERS"),
+		filepath.Join(tempDir, ".github/CONTRIBUTING"),
+	}
+
+	// Execute the function
+	err = template.Execute(tempDir, settings)
+	require.NoError(t, err)
+
+	// Check if the files were created
+	for _, filePath := range expectedFilePaths {
+		_, err := os.Stat(filePath)
+		require.NoError(t, err)
+	}
+
+	// Clean up the temporary directory
+	os.RemoveAll(tempDir)
+
+}
