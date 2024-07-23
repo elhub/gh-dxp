b480c747ede91ef9b4b608944d2e7d986ee81c67
Henrik Fauskanger <henrik.fauskanger@statnett.no>
2024-07-23 14:55:02 +0200

    handle shadowing

--- pkg/pr/pr.go
+++ pkg/pr/pr.go
@@ -497,9 +497,10 @@ func getChanges(exe utils.Executor, re *regexp.Regexp) ([]string, error) {
 
 func addAndCommitFiles(exe utils.Executor, files []string, options *Options) error {
 	var commitMessage string
+	var err error
 
 	if !options.AutoConfirm {
-		commitMessage, err := askForString("Please enter a commit message", "")
+		commitMessage, err = askForString("Please enter a commit message: ", "")
 		if err != nil {
 			return err
 		} else if len(commitMessage) == 0 {
@@ -508,7 +509,6 @@ func addAndCommitFiles(exe utils.Executor, files []string, options *Options) err
 	} else {
 		commitMessage = "default commit message"
 	}
-
 	// Get git root directory and add to files to get fully qualified paths
 	root, err := utils.GetGitRootDirectory(exe)
 	if err != nil {
