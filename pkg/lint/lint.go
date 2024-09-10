ca27a4a5c677a7b9fa995f54b4c171b40b05fce4
Henrik Fauskanger <henrik.fauskanger@statnett.no>
2024-09-10 18:04:24 +0200

    Implement -d/--directory flag in lint command [minor]

--- pkg/lint/lint.go
+++ pkg/lint/lint.go
@@ -3,6 +3,7 @@ package lint
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/caarlos0/log"
@@ -13,6 +14,7 @@ import (
 // Run runs the linting process using megalinter (https://github.com/oxsecurity/megalinter).
 // Megalinter is an open-source linter aggregator that runs multiple linters in parallel. It requires NodeJS (npx) to be installed.
 func Run(exe utils.Executor, _ *config.Settings, opts *Options) error {
+	log.Info(opts.Directory)
 	// Run mega-linter-runner with the cupcake flavor.
 	ctx := context.Background()
 
@@ -24,7 +26,7 @@ func Run(exe utils.Executor, _ *config.Settings, opts *Options) error {
 		// Append the default configuration file to the args.
 		args = append(args, "-e", "MEGALINTER_CONFIG=https://raw.githubusercontent.com/elhub/devxp-lint-configuration/main/resources/.mega-linter.yml")
 	}
-	if !opts.LintAll {
+	if !opts.LintAll && opts.Directory == "" {
 		changedFiles, err := getChangedFiles(exe)
 		if err != nil {
 			return err
@@ -37,11 +39,13 @@ func Run(exe utils.Executor, _ *config.Settings, opts *Options) error {
 
 		args = append(args, "--filesonly")
 		args = append(args, changedFiles...)
+	} else if opts.Directory != "" {
+		args = append(args, "-e", "FILTER_REGEX_INCLUDE="+fmt.Sprintf("(%s)", opts.Directory))
 	}
 	if opts.Fix {
 		args = append(args, "--fix")
 	}
-
+	log.Info(strings.Join(args, ", "))
 	err := exe.CommandContext(ctx, args[0], args[1:]...)
 	if err != nil {
 		log.Info("The Lint Process returned an error: " + err.Error() + "\n")
