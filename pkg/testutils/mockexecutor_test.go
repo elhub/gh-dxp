8d73d84107065731eaae4614d256bf88b34b9fba
Michael Akinde <michael.akinde@statnett.no>
2024-09-18 14:40:00 +0200

    Add automated documentation checks to PR info (#86)
    ## 📝 Description
    
    This change adds automated checks for changes in README and /docs to the
    PR template.
    
    Created testutils.NewMockExecutor to simplify testing of the
    documentation, and added tests for that as well.
    
    ## 🔗 Issue ID(s): TDX-625
    
    ## 📋 Checklist
    
    * ✅ Lint checks passed on local machine.
    * ✅ Unit tests passed on local machine.
    * ✅ This PR adds news tests.

---
+++ pkg/testutils/mockexecutor_test.go
@@ -0,0 +1,59 @@
+package testutils_test
+
+import (
+	"testing"
+
+	"github.com/elhub/gh-dxp/pkg/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMockExecutor(t *testing.T) {
+	mocks := []testutils.MockContent{
+		{
+			Method: "Command",
+			Args:   []interface{}{"git", []string{"rev-parse", "--show-toplevel"}},
+			Out:    "/path/to/repo\n",
+			Err:    nil,
+		},
+		{
+			Method: "CommandContext",
+			Args:   []interface{}{nil, "git", []string{"rev-parse", "--show-toplevel"}},
+			Out:    "",
+			Err:    nil,
+		},
+		{
+			Method: "GH",
+			Args:   []interface{}{[]string{"repo", "view"}},
+			Out:    "mocked output",
+			Err:    nil,
+		},
+		{
+			Method: "Chdir",
+			Args:   []interface{}{"/mock/path"},
+			Out:    "",
+			Err:    nil,
+		},
+	}
+
+	mockExe := testutils.NewMockExecutor(mocks)
+
+	// Test Command method
+	cOutput, err := mockExe.Command("git", "rev-parse", "--show-toplevel")
+	assert.Equal(t, "/path/to/repo\n", cOutput)
+	assert.NoError(t, err)
+
+	// Test CommandContext method
+	err = mockExe.CommandContext(nil, "git", "rev-parse", "--show-toplevel")
+	assert.NoError(t, err)
+
+	// Test GH method
+	ghOutput, err := mockExe.GH("repo", "view")
+	assert.NoError(t, err)
+	assert.Equal(t, "mocked output", ghOutput.String())
+
+	// Test Chdir method
+	err = mockExe.Chdir("/mock/path")
+	assert.NoError(t, err)
+
+	mockExe.AssertExpectations(t)
+}
