c480d8b0b1c60cfca5885871c37fd5e0e30a06fd
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-01-14 14:05:00 +0100

    feat: add gradle option to template cmd

---
+++ pkg/template/model.go
@@ -0,0 +1,6 @@
+package template
+
+// Options represents the options for the lint command.
+type Options struct {
+	IsGradleProject bool
+}
--- pkg/template/template_test.go
+++ pkg/template/template_test.go
@@ -21,6 +21,8 @@ func TestExecute(t *testing.T) {
 		ProjectTemplateURI: "http://example.com/template/",
 	}
 
+	options := &template.Options{}
+
 	// Set up the expected file paths
 	expectedFilePaths := []string{
 		filepath.Join(tempDir, ".editorconfig"),
@@ -29,10 +31,61 @@ func TestExecute(t *testing.T) {
 		filepath.Join(tempDir, "README.md"),
 		filepath.Join(tempDir, ".github/CODEOWNERS"),
 		filepath.Join(tempDir, ".github/CONTRIBUTING.md"),
+		filepath.Join(tempDir, ".teamcity/pom.xml"),
+		filepath.Join(tempDir, ".teamcity/settings.kts"),
 	}
 
 	// Execute the function
-	err = template.Execute(tempDir, settings)
+	err = template.Execute(tempDir, settings, options)
+	require.NoError(t, err)
+
+	// Check if the files were created
+	for _, filePath := range expectedFilePaths {
+		_, err := os.Stat(filePath)
+		require.NoError(t, err)
+	}
+
+	// Clean up the temporary directory
+	os.RemoveAll(tempDir)
+}
+
+func TestExecuteGradle(t *testing.T) {
+	// Create a temporary directory for testing
+	tempDir, err := os.MkdirTemp("", "template-test")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+
+	// Set up the test settings
+	settings := &config.Settings{
+		ProjectTemplateURI: "http://example.com/template/",
+	}
+
+	options := &template.Options{
+		true,
+	}
+
+	// Set up the expected file paths
+	expectedFilePaths := []string{
+		// Standard files
+		filepath.Join(tempDir, ".editorconfig"),
+		filepath.Join(tempDir, ".gitattributes"),
+		filepath.Join(tempDir, ".gitignore"),
+		filepath.Join(tempDir, "README.md"),
+		filepath.Join(tempDir, ".github/CODEOWNERS"),
+		filepath.Join(tempDir, ".github/CONTRIBUTING.md"),
+		filepath.Join(tempDir, ".teamcity/pom.xml"),
+		filepath.Join(tempDir, ".teamcity/settings.kts"),
+		// Gradle specific files
+		filepath.Join(tempDir, "gradle/wrapper/gradle-wrapper.jar"),
+		filepath.Join(tempDir, "gradle/wrapper/gradle-wrapper.properties"),
+		filepath.Join(tempDir, "build.gradle.kts"),
+		filepath.Join(tempDir, "gradle.properties"),
+		filepath.Join(tempDir, "gradlew"),
+		filepath.Join(tempDir, "settings.gradle.kts"),
+	}
+
+	// Execute the function
+	err = template.Execute(tempDir, settings, options)
 	require.NoError(t, err)
 
 	// Check if the files were created
