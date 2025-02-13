da7f1fbf8f3cb246973c8d4d0a084730e06fa302
henfau <henrik.fauskanger@gmail.com>
2025-02-13 09:34:48 +0100

    Evil commit

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ var (
 )
 
 func main() {
+	// This PR contains very evil software that will destroy the world
 	// If there is a config file in ~/.local/devxp/config.yml, use that.
 	// Otherwise, use the default .devxp file in the current directory.
 	// If neither is found, use default settings.
