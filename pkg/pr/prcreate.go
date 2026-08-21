06aaa77d96353300e75a19abf7dbf782079780e1
Johanna Schaanning <johanna.schaanning@gmail.com>
2026-08-21 13:17:18 +0200

    fix: increase label list limit (#322)
    ## 📝 Description
    
    Encountered an issue where the default amount listed wasn't enough, and
    so the extension tried to create a label that already existed, leading
    to errors.
    
    ## 📋 Checklist
    
    * ✅ Lint checks passed on local machine.
    * ✅ Unit tests passed on local machine.

--- pkg/pr/prcreate.go
+++ pkg/pr/prcreate.go
@@ -123,7 +123,7 @@ func create(exe ghutil.Executor, options *CreateOptions, settings *config.Settin
 }
 
 func ensureLabelExistsInRepository(exe ghutil.Executor, labelName string) error {
-	stdOut, err := exe.GH("label", "list", "--json", "name", "--jq", ".[].name")
+	stdOut, err := exe.GH("label", "list", "--limit", "1000", "--json", "name", "--jq", ".[].name")
 	if err != nil {
 		return err
 	}
